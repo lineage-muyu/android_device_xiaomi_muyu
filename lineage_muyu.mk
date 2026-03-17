4c562187e4a906bbda2a58f5e04d2d1171c3c861
Gerald LATKOVIC <gerald@latkovic.fr>
2026-03-17 22:19:06 +0100

    lineage_muyu: use modern BuildDesc/BuildFingerprint/DeviceProduct style

--- lineage_muyu.mk
+++ lineage_muyu.mk
@@ -24,8 +24,7 @@ PRODUCT_MANUFACTURER := xiaomi
 # GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# Build fingerprint from stock CN firmware OS3.0.11.0.WOYCNXM
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="muyu-user 14 UKQ1.240624.001 OS3.0.11.0.WOYCNXM release-keys"
-
-BUILD_FINGERPRINT := Xiaomi/muyu/muyu:14/UKQ1.240624.001/OS3.0.11.0.WOYCNXM:user/release-keys
+    BuildDesc="muyu-user 14 UKQ1.240624.001 OS3.0.11.0.WOYCNXM release-keys" \
+    BuildFingerprint=Xiaomi/muyu/muyu:14/UKQ1.240624.001/OS3.0.11.0.WOYCNXM:user/release-keys \
+    DeviceProduct=muyu
